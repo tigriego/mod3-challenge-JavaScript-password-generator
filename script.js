ba02453b2522ca91df93e7f7b82da2b59e77f692
Ti Griego <128423140+tigriego@users.noreply.github.com>
2023-05-11 11:36:52 -0500

    add return for generated password

--- script.js
+++ script.js
@@ -4,6 +4,20 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword () {
+  console.log("Button has been clicked!")
+
+//1. Prompt the user for the password critera
+//  a. Password length between 8-128
+//  b. lowercase, uppercase, numeric, and/or special characters
+//2. Validate the input
+//3. Generate password based on critera
+
+
+//4. Display the generated password on page
+return "Generated Password will go here!";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
